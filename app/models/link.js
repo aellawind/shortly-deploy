36f2278b21f1ebdb89880a666470932e3f3b26b1
Amira Anuar <amiraanuar@gmail.com>
2014-05-14 22:14:30 -0700

    Begin refactoring using mongo db

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,29 @@
 var db = require('../config');
 var crypto = require('crypto');
+var mongoose = require('mongoose');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var urlSchema = mongoose.Schema({
+  url: {type:String,unique:true},
+  base_url: String,
+  code: String,
+  title: String,
+  timestamp: { type : Date, default: Date.now },
+  visits: {type: Number, default: 0}
 });
 
-module.exports = Link;
+urlSchema.methods.init = function() {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+};
+
+// userSchema.pre('save', function(next) {
+//   var user = this;
+//   user.hashPassword();
+//   next();
+// });
+
+var Link = mongoose.model('Link', urlSchema);
+
+module.exports = mongoose.model('Link', urlSchema);
+
--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,34 @@
 var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
+var mongoose = require('mongoose');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+var userSchema = mongoose.Schema({
+    username: {type:String,unique:true},
+    password: String,
+    timestamp: { type: Date, default: Date.now }
 });
 
-module.exports = User;
+
+userSchema.methods.comparePassword = function(attemptedPassword, cb) {
+  bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
+    cb(err,isMatch);
+  });
+};
+
+userSchema.methods.hashPassword = function() {
+  var cipher = Promise.promisify(bcrypt.hash);
+  return cipher(this.password, null,null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      console.log("PW",this.password);
+    });
+}
+
+userSchema.pre('save', function(next) {
+  var user = this;
+  user.hashPassword();
+  next();
+});
+
+module.exports = mongoose.model('User',userSchema);
